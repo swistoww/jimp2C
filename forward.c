ee1df7fccb7349b9ac884c3a0ead23f539513a79
swistoww <swistowski.w@gmail.com>
2016-04-22 15:14:55 +0200

    wstęp implementacji forward

--- forward.c
+++ forward.c
@@ -1,48 +1,54 @@
 #include "readData.h"
 #include "forward.h"
 
-struct ar *forwardThinking(struct ar *dataBank, struct ru *ruleBank, char **arguments){
+struct ar *forwardThinking(struct ar *dataBank, struct ru *ruleBank){
     int i;
     int length;
-    unsigned long ruleIdx, dataIdx;
+    unsigned long ruleIdx;
     char *word;
-    char *token;
-    char *operator;
     for (i=0; i < length; i++){
         int k = 1;
         int value1, value2;
-        word = *(*arguments+i);
+        word = **(arguments+i);
         ruleIdx = hash(word);
         if(word =! ruleBank[ruleIdx].consequent){
             printf("Dane sa niekompletne. Brak danej %s\n", word);
             exit(1);
         }
         else{
-            token = strtok(ruleBank[ruleIdx].atecendent[i], " ");
-            while(token != NULL){
-                //usuwamy nawias otwierający
-                dataIdx = hash(token);
-                if (k=1){
-                    value1 = dataBank[dataIdx].value;
-                }
-                //jezeli token ma nawias otwierający {
-                //rekurencja z tokenem }
-                operator = strtok(NULL, " ");
-                token = strtok(NULL, " ");
-                //jeżeli nawias zamykający {
-                //usuwamy nawias z tokena }
-                dataIdx = hash(token);
-                value2 = dataBank[dataIdx].value;
-                if (strcmp(operator, "&&") == 0){
-                    value1 = (value1 && value2);
-                }
-                if (strcmp(operator, "||") == 0) {
-                    value1 = (value1 && value2);
-                }
-            }
-            if (dataBank[ruleIdx].value == NULL){
+            char *tmpRule = strdup(ruleBank[ruleIdx].atecendent[i]);
+
+            if (dataBank[ruleIdx].value == NULL){ //wstaiwenie do tablicy danych obliczonej wartosci
                 dataBank[ruleIdx].value = value1;
             }
         }
     }
+}
+
+int simpleThinking (char *rule, struct ar *dataBank, struct ru *ruleBank){
+    int value1, value2, k;
+    unsigned long dataIdx;
+    char *operator;
+    char *token, *tmpRule;
+    while(1){
+        token = strtok(tmpRule, " ");
+        dataIdx = hash(token);
+        if (k=1){
+            value1 = dataBank[dataIdx].value;
+            operator = strtok(NULL, " ");
+            token = strtok(NULL, " ");
+            dataIdx = hash(token);
+        }
+        value2 = dataBank[dataIdx].value;
+        if (strcmp(operator, "&&") == 0){
+            value1 = (value1 && value2);
+        }
+        if (strcmp(operator, "||") == 0) {
+            value1 = (value1 && value2);
+        }
+        k++;
+        if (operator = strtok(NULL, " ") == NULL);
+        break;
+    }
+    return value1;
 }
